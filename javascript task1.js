77440627001a3a9094302263d2a79d313b0a2b5e
lemmybe <yomshadeemmans@gmail.com>
2020-03-25 17:48:43 +0100

    corrected

--- javascript task1.js
+++ javascript task1.js
@@ -3,7 +3,7 @@ var courses= ["html","css","javascript","uiux"];
 // displaying the name 
 console.log("my name is " + name);
 // displaying the courses
-console.log("my courses are " + courses);
+console.log("my courses are: " + courses);
 // displaying even numbers (my courses are 4)
 for (let x=1; x<=200; x++) {
  if (x % 2==0) console.log(x);
