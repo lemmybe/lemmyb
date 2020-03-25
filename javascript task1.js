5b36df7fa53b589b30d7ec55b7fbc62aa5fd999f
lemmybe <yomshadeemmans@gmail.com>
2020-03-25 17:18:35 +0100

    mytask3

---
+++ javascript task1.js
@@ -0,0 +1,11 @@
+var name="omisade emmanuel";
+var courses= ["html","css","javascript","uiux"];
+// displaying the name 
+console.log("my name is " + name);
+// displaying the courses
+console.log("my courses are " + courses);
+// displaying even numbers (my courses are 4)
+for (let x=1; x<=200; x++) {
+ if (x % 2==0) console.log(x);
+}
+
